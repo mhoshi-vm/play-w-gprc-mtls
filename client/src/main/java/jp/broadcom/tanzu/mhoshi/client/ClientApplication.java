c4fe92b77f4d7f92cd91c8f8f8498397cacff3d2
mhoshi-vm <machi.hoshino@broadcom.com>
2026-01-09 12:30:07 +0900

    grpc with mtls play

--- client/src/main/java/jp/broadcom/tanzu/mhoshi/client/ClientApplication.java
+++ client/src/main/java/jp/broadcom/tanzu/mhoshi/client/ClientApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.grpc.client.GrpcChannelFactory;
 
+import java.util.Iterator;
+
 @SpringBootApplication
 public class ClientApplication implements CommandLineRunner {
 
@@ -25,8 +27,25 @@ public class ClientApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        HelloReply reply = stub.sayHello(HelloRequest.newBuilder().setName("hello").build());
-        System.out.println(reply.getMessage());
+        System.out.println(stub.sayHello(HelloRequest.newBuilder().setName("hello").build()));
+        Iterator<HelloReply> iterator = stub.streamHello(HelloRequest.newBuilder().setName("hello2").build());
+        while (iterator.hasNext()) {
+            // Read the current message and advance the iterator
+            String message = iterator.next().getMessage();
+            System.out.println("Message: " + message);
+        }
+
+        try {
+            System.out.println(stub.sayHello(HelloRequest.newBuilder().setName("error").build()));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+          System.out.println(stub.sayHello(HelloRequest.newBuilder().setName("internal").build()));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
 
